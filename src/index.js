52f33738f31000e1a5c760ea1d5da1f7fcbdd72c
Emilio <marqetm@gmail.com>
2022-06-17 20:56:59 -0500

    commit tree af4f0d97fa75be4ca40c84481af933bac319c477

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+
 let addToy = false;
+const toyForm = document.querySelector('.container')
+const addBtn = document.querySelector('#new-toy-btn')
+let collection = document.querySelector('#toy-collection')
+let likeValue = 0
+//Event listener for form
+
+
+//Event handlers
+// function handleSubmit(e) {
+//   e.preventDefault()
+//   let
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +26,60 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  //Render DOM
+  function renderOneToy(toy) {
+    const card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+    <div>
+      <h2>${toy.name}</h2>
+    </div>
+    <img src="${toy.image}" class="toy-avatar">
+    <p>${toy.likes} likes</p>
+  `
+    //Like button
+    const likeBtn = document.createElement('button')
+    likeBtn.innerHTML = 'like'
+    card.appendChild(likeBtn)
+    likeBtn.addEventListener('click', handleLikes)
+    collection.appendChild(card)
+  }
+
+
+
+  //Fetch toys
+  const BASE_URL = "http://localhost:3000/toys"
+
+  function getToys() {
+    return fetch(BASE_URL)
+      .then(resp => resp.json())
+      .then(toys => toys.forEach(toy => renderOneToy(toy)))
+      .catch(function () {
+        alert('Cannot find requested toy list.')
+      })
+  }
+  getToys()
+
+  //POST likes
+  function handleLikes() {
+    fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': "application/json",
+        "Accept": "application/json"
+      },
+      Body: JSON.stringify(toy)
+    })
+      .then(resp => resp.json())
+      .then(toy => console.log(toy.likes))
+      .catch(function () {
+        alert('Cannot find requested toy list.')
+      })
+
+  }
+})
+
+
+
